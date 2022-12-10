0bf44019e53d6730610de3fc08843b4580257b7e
ProwlingLynx <tamezjavier@gmail.com>
2022-12-10 12:53:22 -0600

    Implement and test mapRoundStringToGameRoundObject.

--- src/main/java/filereader/GameInputReader.java
+++ src/main/java/filereader/GameInputReader.java
@@ -1,7 +1,12 @@
 package filereader;
 
+import rounddata.GameRound;
+import rounddata.IGameRound;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class GameInputReader {
@@ -14,5 +19,20 @@ public class GameInputReader {
         while(sc.hasNext()){
             System.out.println(sc.nextLine());
         }
+    }
+    public List<IGameRound> mapRoundStringToGameRoundObject() {
+        List<IGameRound> gameRounds = new ArrayList<>();
+        while (sc.hasNext()){
+            String roundString = sc.nextLine();
+            String elf = roundString.substring(0,1);
+            String you = roundString.substring(2, 3);
+            System.out.println(String.format(
+                    "Elf used: %s!\nYou used: %s!",
+                    elf,
+                    you
+            ));
+            gameRounds.add(new GameRound(elf, you));
+        }
+        return gameRounds;
     }
 }
--- src/main/java/filereader/TestGameInputReader.java
+++ src/main/java/filereader/TestGameInputReader.java
@@ -1,8 +1,13 @@
 package filereader;
 
+import rounddata.IGameRound;
+
+import java.util.List;
+
 class TestGameInputReader {
     public static void main(String[] args) {
         testFileReaderMethod();
+        testMapRoundStringToGameRoundObject();
 
     }
     private static void testFileReaderMethod(){
@@ -10,6 +15,17 @@ class TestGameInputReader {
             System.out.println("Testing file reader. Should not throw an exception");
             GameInputReader reader = new GameInputReader("src/main/resources/sampleGameRounds.txt");
             reader.readFile();
+        } catch (Exception e) {
+            System.out.println("Test failed, exception thrown");
+        }
+    }
+
+    private static void testMapRoundStringToGameRoundObject() {
+        try {
+            System.out.println("Testing file reader. Should not throw an exception");
+            GameInputReader reader = new GameInputReader("src/main/resources/sampleGameRounds.txt");
+            List<IGameRound> testList = reader.mapRoundStringToGameRoundObject();
+            System.out.println(String.format("Expect testList to be of size 3.\n We got size: %s", testList.size()));
         } catch (Exception e) {
             System.out.println("Test failed, exception thrown");
         }
